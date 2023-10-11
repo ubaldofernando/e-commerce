f46b2f502db090473a7b6f07673023c424e09c33
ubaldofernando <ubaldofernando@gmail.com>
2023-10-11 16:13:40 -0300

    agrego estilos

---
+++ js/main.js
@@ -0,0 +1,176 @@
+// Array de productos
+const productos = [
+    // Camperas
+    {
+        id: "campera-01",
+        titulo: "Campera 01",
+        imagen: "./img/camperas/01.jpg",
+        categoria: {
+            nombre: "Camperas",
+            id: "camperas"
+        },
+        precio: 17000
+    },
+    {
+        id: "campera-02",
+        titulo: "Campera 02",
+        imagen: "./img/camperas/02.jpg",
+        categoria: {
+            nombre: "Camperas",
+            id: "camperas"
+        },
+        precio: 13200
+    },
+    {
+        id: "campera-03",
+        titulo: "Campera 03",
+        imagen: "./img/camperas/03.jpg",
+        categoria: {
+            nombre: "Camperas",
+            id: "camperas"
+        },
+        precio: 19400
+    },
+    {
+        id: "campera-04",
+        titulo: "Campera 04",
+        imagen: "./img/camperas/04.jpg",
+        categoria: {
+            nombre: "Camperas",
+            id: "camperas"
+        },
+        precio: 20690
+    },
+    {
+        id: "campera-05",
+        titulo: "Campera 05",
+        imagen: "./img/camperas/05.jpg",
+        categoria: {
+            nombre: "Camperas",
+            id: "camperas"
+        },
+        precio: 30100
+    },
+    // Remeras
+    {
+        id: "remera-01",
+        titulo: "Remera 01",
+        imagen: "./img/remeras/01.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+        precio: 5190
+    },
+    {
+        id: "remera-02",
+        titulo: "Remera 02",
+        imagen: "./img/remeras/02.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+        precio: 6900
+    },
+    {
+        id: "remera-03",
+        titulo: "Remera 03",
+        imagen: "./img/remeras/03.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+        precio: 7000
+    },
+    {
+        id: "remera-04",
+        titulo: "Remera 04",
+        imagen: "./img/remeras/04.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+        precio: 3800
+    },
+    {
+        id: "remera-05",
+        titulo: "Remera 05",
+        imagen: "./img/remeras/05.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+        precio: 3900
+    },
+    {
+        id: "remera-06",
+        titulo: "Remera 06",
+        imagen: "./img/remeras/06.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+        precio: 6200
+    },
+    {
+        id: "remera-07",
+        titulo: "Remera 07",
+        imagen: "./img/remeras/07.jpg",
+        categoria: {
+            nombre: "Remeras",
+            id: "remeras"
+        },
+        precio: 3600
+    },
+    // Pantalones
+    {
+        id: "pantalon-01",
+        titulo: "Pantalón 01",
+        imagen: "./img/pantalones/01.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 8699
+    },
+    {
+        id: "pantalon-02",
+        titulo: "Pantalón 02",
+        imagen: "./img/pantalones/02.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 6490
+    },
+    {
+        id: "pantalon-03",
+        titulo: "Pantalón 03",
+        imagen: "./img/pantalones/03.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 3000
+    },
+    {
+        id: "pantalon-04",
+        titulo: "Pantalón 04",
+        imagen: "./img/pantalones/04.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 6800
+    },
+    {
+        id: "pantalon-05",
+        titulo: "Pantalón 05",
+        imagen: "./img/pantalones/05.jpg",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 3000
+    }
+];
